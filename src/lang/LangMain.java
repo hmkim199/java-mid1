18077d0b81eaae8dea7f72e7c623db1bc48ff027
hmkim199 <hmkim199@gmail.com>
2024-12-04 23:04:12 +0900

    섹션2

---
+++ src/lang/LangMain.java
@@ -0,0 +1,9 @@
+package lang;
+
+import java.lang.System; // 생략 가능. 너무 자주 쓰는 기능들이라 자동 import됨
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
